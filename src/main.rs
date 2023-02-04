e5c6ad022c6a593ce0731b57d3d0ed8be55d19c7
Jason Song <jasonsong.93@gmail.com>
2023-02-05 02:24:02 +1100

    Updated so that invalid inputs would be ignored. Game finished

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ fn main() {
     println!("Guess the number");
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
-
     loop {
         println!("Please input your guess:");
 
@@ -17,10 +15,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess
-            .trim()
-            .parse()
-            .expect("Conversion error, please type a valid number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {guess}");
 
